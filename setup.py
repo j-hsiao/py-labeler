a4ec6694980ad438fdea037a7585f5994dde1097
jason hsiao <oaishnosaj@gmail.com>
2022-09-05 17:38:08 +0100

    update setup.py for newest setuptools
    newest setuptools fails namespace package unless it is explicitly added

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author='Jason Hsiao',
     author_email='oaishnosaj@gmail.com',
     description='Label sets of images.',
-    packages=['jhsiao.labeler', 'jhsiao.labeleritems'],
+    packages=['jhsiao', 'jhsiao.labeler', 'jhsiao.labeleritems'],
     install_requires=['jhsiao-tkutil @ git+https://github.com/j-hsiao/py-tkutil.git']
 )
