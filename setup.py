02f4f7abc35d832f9b31f9d6cf2d7458bbec8d07
Hsiao <Jason.Hsiao@acer.com>
2022-09-05 10:09:54 +0100

    change names to use pep503 normalized names (. become -)

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from jhsiao.namespace import make_ns, fdir
 make_ns('jhsiao', 'jhsiao/labeleritems', dir=fdir(__file__))
 setup(
-    name='jhsiao.labeler',
+    name='jhsiao-labeler',
     version='0.0.1',
     author='Jason Hsiao',
     author_email='oaishnosaj@gmail.com',
     description='Label sets of images.',
     packages=['jhsiao.labeler', 'jhsiao.labeleritems'],
-    install_requires=['jhsiao.tkutil @ git+https://github.com/j-hsiao/py-tkutil.git']
+    install_requires=['jhsiao-tkutil @ git+https://github.com/j-hsiao/py-tkutil.git']
 )
